d03f55d335b9eded140f8df6cab1a2a6ab5ae479
hernan066 <hernan0066@gmail.com>
2021-10-13 12:59:24 -0300

    3. config redux

--- src/Agenda.jsx
+++ src/Agenda.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import { Provider } from 'react-redux'
 import AppRouter from './components/routers/AppRouter'
+import { store } from './store/store'
 
 const Agenda = () => {
     return (
-        <div>
+        <Provider store = {store}>
             <AppRouter />
+        </Provider>
+
             
-        </div>
+        
     )
 }
 
