b9028a9bd91555faa9fa110fb4f52e5679398190
hernan066 <hernan0066@gmail.com>
2021-10-16 12:50:35 -0300

    17. LoadinScreen global

---
+++ src/components/auth/LoadinScreen.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const LoadinScreen = () => {
+    return (
+        <div className="auth__main">
+            <div className="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+          
+        </div>
+    )
+}
+
+export default LoadinScreen
--- src/components/routers/AppRouter.jsx
+++ src/components/routers/AppRouter.jsx
@@ -1,10 +1,10 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import AuthRouter from './AuthRouter';
 import {
     BrowserRouter as Router,
     Switch,
-   
+    Redirect,
     Route
   } from 'react-router-dom';
 import JournalScreen from '../journal/JournalScreen';
@@ -12,45 +12,58 @@ import { app } from '../../firebase/firebase-config';
 import { getAuth, onAuthStateChanged } from '@firebase/auth';
 import { useDispatch } from 'react-redux';
 import { login } from '../actions/auth';
+import LoadinScreen from '../auth/LoadinScreen';
 //import { PublicRoute } from './PublicRoute';
 
 
 const AppRouter = () => {
     
     const auth = getAuth(app);
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
+
+    const [checking, setChecking] = useState(true);
+    const [isLoging, setIsLoging] = useState(false);
     
     useEffect(() => {
         onAuthStateChanged(auth, (user)=>{
             //user?.uid si el objeto user tiene algo (no null), pregunta si existe el uid
             if(user?.uid){
                 dispatch(login(user.uid, user.displayName));
+                setIsLoging(true);
+            }else{
+                setIsLoging( false)
             }
+            setChecking(false);
         });
       
-    }, [dispatch, auth]);
+    }, [dispatch, auth, setChecking, setIsLoging]);
     
-    
+    if (checking){
+        return(
+            <LoadinScreen />
+           
+        )
+    }
     
     return (
         
             <Router>
                 <div>
                     <Switch>
-                    <Route 
-                        path="/auth"
-                        component={ AuthRouter }
-                        
-                    />
-                    <Route 
-                        path="/"
-                        component={ JournalScreen }
-                        exact
-                        
-                    />
+                        <Route 
+                            path="/auth"
+                            component={ AuthRouter }
+                            
+                        />
+                        <Route 
+                            path="/"
+                            component={ JournalScreen }
+                            exact
+                            
+                        />
+                        {<Redirect to="/auth/login" />}
                     </Switch>
 
-                    {/* <Redirect to="/auth/login" /> */}
                 
                 </div>
             </Router>
