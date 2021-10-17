35f2459595bfab97a3285c7d8369a9a3160db2f9
hernan066 <hernan0066@gmail.com>
2021-10-17 11:07:58 -0300

    2.6 activar nota creada

--- src/components/actions/notes.js
+++ src/components/actions/notes.js
@@ -7,6 +7,7 @@
 
 import { getFirestore, collection, addDoc } from "firebase/firestore";
 import { app } from "../../firebase/firebase-config";
+import { types } from "../../types/types";
 
 export const startNewNote = ()=> {
     return async ( dispatch, getState)=> {
@@ -24,5 +25,15 @@ export const startNewNote = ()=> {
        const docRef = await addDoc(collection(db, `${uid}/journal/notes`), newNote); //firebase 9
 
        console.log(docRef)
+
+       dispatch (activeNote(docRef.id, newNote ))
     }
 }
+
+export const activeNote = (id, note)=>({
+    type: types.notesActive,
+    payload: {
+        id, 
+        ...note
+    }
+})
--- src/reducers/notesReducer.js
+++ src/reducers/notesReducer.js
@@ -1,3 +1,5 @@
+import { types } from "../types/types";
+
 const initialState = {
     notes: [],
     active : null,
@@ -6,7 +8,13 @@ const initialState = {
 
 export const notesReducer = (state = initialState, action)=>{
     switch (action.type) {
-        
+        case types.notesActive:
+            return{
+                ...state,
+                active: {
+                    ...action.payload
+                }
+            }
     
         default:
             return state;
